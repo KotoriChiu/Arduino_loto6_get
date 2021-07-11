2c3d15e0e9e89ccbdb4dce72dce3e7701d5ae9ec
KotoriChiu <zxcv88928@gmail.com>
2021-07-12 03:17:29 +0800

    got it

--- loto_get.ino
+++ loto_get.ino
@@ -1,6 +1,5 @@
 #include "WiFiEsp.h"
 #include "TimedAction.h"
-#ifndef HAVE_HWSERIAL1
 
 char ssid[] = "P883-cht-3";           
 char pass[] = "as0955065969";   
@@ -9,7 +8,7 @@ String text = "";
 int LOTOtoINT[22]={0};
 WiFiEspClient client;
 char server[] = "lotophp.000webhostapp.com";
-int red_led = 9,green_led = 10
+int red_led = 9,green_led = 10;
 unsigned long lastConnectionTime = 0;         
 const unsigned long postingInterval = 10000L; 
 
@@ -20,6 +19,7 @@ void setup(){
     digitalWrite(green_led,LOW);
     Serial.begin(115200);
     Serial1.begin(115200);
+    Serial2.begin(9600);
     WiFi.init(&Serial1);
     if (WiFi.status() == WL_NO_SHIELD) {
     Serial.println("WiFi shield not present");
@@ -45,7 +45,7 @@ void loop(){
     while(client.available()){
         get_data = client.read();
         text = String(get_data);
-        Serial.print(text);
+        //Serial.print(text);
         if(text == "$" || HE_ == true){
             if(HE_ == false){
                 HE_ = true;
@@ -60,7 +60,10 @@ void loop(){
             loto_vlaue++;
         }
     }
+     Serial2.print(LOTOtoINT[0]+""+LOTOtoINT[1]+""+LOTOtoINT[2]+""+LOTOtoINT[3]+""+LOTOtoINT[4]+""+LOTOtoINT[5]+""+LOTOtoINT[6]+""+LOTOtoINT[7]+""+LOTOtoINT[8]+""+LOTOtoINT[9]+""+LOTOtoINT[10]+""+LOTOtoINT[11]+""+LOTOtoINT[12]+""+LOTOtoINT[13]+""+LOTOtoINT[14]+""+LOTOtoINT[15]+""+LOTOtoINT[16]+""+LOTOtoINT[17]+""+LOTOtoINT[18]+""+LOTOtoINT[19]+""+LOTOtoINT[20]+""+LOTOtoINT[21]+"\n");
      loto_vlaue=0;
+    if (millis() - lastConnectionTime > postingInterval) httpRequest();
+
 }
 
 void printWifiStatus(){
